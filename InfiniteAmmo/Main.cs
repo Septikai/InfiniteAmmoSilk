881a4d97e99754928131c545359f9f62d5f43b21
Natalia Spence <52349324+Septikai@users.noreply.github.com>
2024-04-09 15:52:55 +0100

    1.3.0 - update for modweaver

--- InfiniteAmmo/Main.cs
+++ InfiniteAmmo/Main.cs
@@ -1,32 +1,45 @@
-﻿using BepInEx;
-using HarmonyLib;
+﻿using HarmonyLib;
+using modweaver.core;
 using UnityEngine;
 
 namespace InfiniteAmmo
 {
-    [BepInPlugin(ModGUID, ModName, ModVersion)]
-    public class Main : BaseUnityPlugin
+    [ModMainClass]
+    public class Main : Mod
     {
-        public const string ModName = "InfiniteAmmo";
-        public const string ModAuthor  = "Septikai";
-        public const string ModGUID = "me.septikai.InfiniteAmmo";
-        public const string ModVersion = "1.2.0";
-        internal Harmony Harmony;
-        
-        internal void Awake()
+        public override void Init()
         {
-            Harmony = new Harmony(ModGUID);
+            // your manifest is the mw.mod.toml file
+            // use Metadata to access the values you provided in the manifest. Manifest is also available, and provides the other data such as your dependencies and incompats
+            Logger.Info("Loading {0} v{1} by {2}!", Metadata.title, Metadata.version,
+                string.Join(", ", Metadata.authors));
+            
+            // this section currently patches any [HarmonyPatch]s you use, like the one named NoMoreLaserCubes below. if you don't patch anything, you can remove these
+            // you should keep the logging messages as they help users and developers with debugging
+            Logger.Debug("Setting up patcher...");
+            Harmony harmony = new Harmony(Metadata.id); 
+            Logger.Debug("Patching...");
+            harmony.PatchAll();
+        }
 
-            Harmony.PatchAll();
-            Logger.LogInfo($"{ModName} successfully loaded! Made by {ModAuthor}");
+        public override void Ready()
+        {
+            Logger.Info("Loaded {0}!", Metadata.title);
+        }
+
+        public override void OnGUI(ModsMenuPopup ui)
+        {
+            // you can add data to your mods page here
+            // we recommend if you are going to add ui here, put
+            // ui.CreateDivider() first
+            // you'll see why :3
         }
     }
 
     [HarmonyPatch(typeof(Weapon), nameof(Weapon.ammo), MethodType.Getter)]
     internal class InfiniteAmmoPatch
     {
-        [HarmonyPrefix]
-        public static bool InfiniteAmmo(Weapon __instance, ref float __result)
+        public static bool Prefix(Weapon __instance, ref float __result)
         {
             __result = __instance.maxAmmo;
             return false;
@@ -36,8 +49,7 @@ namespace InfiniteAmmo
     [HarmonyPatch(typeof(Flare), nameof(Flare.FixedUpdate))]
     internal class InfiniteFlarePatch
     {
-        [HarmonyPrefix]
-        public static void InfiniteFlarePostfix(Flare __instance)
+        public static void Prefix(Flare __instance)
         {
             __instance._burnsStart = Time.time + __instance.ammo;
         }
